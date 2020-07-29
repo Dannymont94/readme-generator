0070dcef02120a4a8c1b85a190e3798dedde45df
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-29 01:39:58 -0400

    converted init into IIFE. succesfully sending answers to generateMarkdown file and successfully writing a readme in the dist folder.

--- index.js
+++ index.js
@@ -3,109 +3,115 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
-function askQuestions() {
-    return inquirer.prompt([
-        // Title of Project (required)
-        {
-            name: 'title',
-            message: 'What is the title of your project?',
-            type: 'input',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the title of your project!');
-                    return false;
-                }
+const questions = [
+    // Title of Project (required)
+    {
+        name: 'title',
+        message: 'What is the title of your project?',
+        type: 'input',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the title of your project!');
+                return false;
             }
-        },
-        // Credits (required)
-        {
-            name: 'credits',
-            message: 'Enter the names of those who have contributed to the project.',
-            type: 'input',
-            validate: creditsInput => {
-                if (creditsInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the names of those who contributed to the project!')
-                }
-            }
-        },
-        // GitHub Username (required)
-        {
-            name: 'github',
-            message: 'Enter your GitHub username.',
-            type: 'input',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!')
-                }
-            }
-        },
-        // Email Address (required)
-        {
-            name: 'email',
-            message: 'Enter your email address.',
-            type: 'input',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!')
-                }
-            }
-        },
-        // Description (required)
-        {
-            name: 'description',
-            message: 'Enter a description for your project.',
-            type: 'input',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a description for your project!');
-                    return false;
-                }
-            }
-        },
-        // Installation
-        {
-            name: 'installation',
-            message: 'What are the steps required to install your project, if any?',
-            type: 'input'
-        },
-        // Usage
-        {
-            name: 'usage',
-            message: 'Provide some instructions and examples for how your project is used.',
-            type: 'input'
-        },
-        // License (provide list of options) (need to generate a badge near the top of the readme for selected license)
-        {
-            name: 'license',
-            message: 'Enter a license for your project.',
-            type: 'input',
-            default: 'MIT'
-        },
-        // Contributing
-        {
-            name: 'contributing',
-            message: 'How can others contribute to this project?',
-            type: 'input'
-        },
-        // Tests
-        {
-            name: 'tests',
-            message: 'Provide examples on how to run the tests provided for this project.',
-            type: 'input'
         }
-        // Table of Contents (generate links based on names of key value pairs in answers object)
-    ]);
-}
+    },
+    // Credits (required)
+    {
+        name: 'credits',
+        message: 'Enter the names of those who have contributed to the project.',
+        type: 'input',
+        validate: creditsInput => {
+            if (creditsInput) {
+                return true;
+            } else {
+                console.log('Please enter the names of those who contributed to the project!')
+            }
+        }
+    },
+    // GitHub Username (required)
+    {
+        name: 'github',
+        message: 'Enter your GitHub username.',
+        type: 'input',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!')
+            }
+        }
+    },
+    // Email Address (required)
+    {
+        name: 'email',
+        message: 'Enter your email address.',
+        type: 'input',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!')
+            }
+        }
+    },
+    // Description (required)
+    {
+        name: 'description',
+        message: 'Enter a description for your project.',
+        type: 'input',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description for your project!');
+                return false;
+            }
+        }
+    },
+    // Installation
+    {
+        name: 'installation',
+        message: 'What are the steps required to install your project, if any?',
+        type: 'input'
+    },
+    // Usage
+    {
+        name: 'usage',
+        message: 'Provide some instructions and examples for how your project is used.',
+        type: 'input'
+    },
+    // License (provide list of options) (need to generate a badge near the top of the readme for selected license)
+    {
+        name: 'license',
+        message: 'Enter a license for your project.',
+        type: 'input',
+        default: 'MIT',
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please enter a license for your project!');
+                return false;
+            }
+        }
+    },
+    // Contributing
+    {
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        type: 'input'
+    },
+    // Tests
+    {
+        name: 'tests',
+        message: 'Provide examples on how to run the tests provided for this project.',
+        type: 'input'
+    }
+    // Table of Contents (generate links based on names of key value pairs in answers object)
+];
 
 const mockAnswers = {
     title: 'readme generator',
@@ -121,14 +127,17 @@ const mockAnswers = {
 };
 
 // function to write README file
-function writeToFile(fileName, data) {
-    // fs.writeFile(readme, data)
+function writeToFile(markdown) {
+    fs.promises.writeFile('./dist/readme.md', markdown)
 }
 
-askQuestions()
-    .then(answers => console.log(mockAnswers))
-// call generateMarkdown(answers)
-// call writeToFile(fileName, data)
-// console.log something to let user know process was successful
-// catch errors
-    .catch(err => console.log(err));
+(async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const markdown = generateMarkdown(answers);
+        writeToFile(markdown);
+        console.log('Readme successfully generated in dist folder!')
+    } catch (err) {
+        console.log(err);
+    }
+})();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
+
+## Description
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+// {
+//     title: 'readme generator', (REQUIRED)
+//     credits: 'Daniel Monterrosa', (REQUIRED)
+//     github: 'Dannymont94', (REQUIRED)
+//     email: 'dannym94@gmail.com', (REQUIRED)
+//     description: 'It generates readme files for your projects.', (REQUIRED)
+//     installation: 'Clone repo, install dependencies with node.', (OPTIONAL)
+//     usage: 'Run index with node and answer questions to generate a readme file.', (OPTIONAL)
+//     license: 'MIT', (REQUIRED)
+//     contributing: 'Please provide credit.', (OPTIONAL)
+//     tests: 'Run them as you please.' (OPTIONAL)
+// };
